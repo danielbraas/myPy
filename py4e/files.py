f4c6565f58f3d26e6af21617ab146fbee170a82a
Daniel Braas <danielbraas@gmail.com>
2020-09-06 12:00:02 -0500

    added stylesheet file

--- py4e/files.py
+++ py4e/files.py
@@ -17,12 +17,44 @@ for line in fhand:
 
 fhand = open('mbox-short.txt')
 for line in fhand:
-line = line.rstrip()
-if line.startswith('From:'):
-print(line)        
+    line = line.rstrip()
+    if line.startswith('From:'):
+        print(line)        
 
 count = 0
 for letter in inp:
     count += 1
 print(count)
 
+fhand = open('mbox-short.txt')
+for line in fhand:
+    line = line.rstrip()
+    if line.find('@uct.ac.za') == -1: continue
+    print(line)
+
+fname = input('Enter the file name: ')
+fhand = open(fname)
+count = 0
+for line in fhand:
+    if line.startswith('Subject:'):
+        count = count + 1
+print('There were', count, 'subject lines in', fname)    
+
+fname = input('Enter the file name: ')
+try:
+    fhand = open(fname)
+except:
+    print('File cannot be opened:', fname)
+    exit()
+count = 0
+for line in fhand:
+    if line.startswith('Subject:'):
+        count = count + 1
+print('There were', count, 'subject lines in', fname)
+
+fout = open('output.txt', 'w')
+line1 = "This here's the wattle,\n"
+fout.write(line1)
+line2 = 'the emblem of our land.\n'
+fout.write(line2)
+fout.close()
