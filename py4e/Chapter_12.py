ea37878855da2f6f3b07faa9a6e972d4df4b74ef
Daniel Braas <danielbraas@gmail.com>
2020-09-12 22:16:45 -0500

    Added some html processing

--- py4e/Chapter_12.py
+++ py4e/Chapter_12.py
@@ -1,4 +1,87 @@
+
 import urllib.request
 fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
 for line in fhand:
-print(line.decode().strip())
+    print(line.decode().strip())
+
+import urllib.request, urllib.parse, urllib.error
+fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
+counts = dict()
+for line in fhand:
+    words = line.decode().split()
+    for word in words:
+        counts[word] = counts.get(word, 0) + 1
+print(counts)    
+
+import urllib.request, urllib.parse, urllib.error
+img = urllib.request.urlopen('http://data.pr4e.org/cover3.jpg').read()
+fhand = open('cover3.jpg', 'wb')
+fhand.write(img)
+fhand.close()
+
+import urllib.request, urllib.parse, urllib.error
+img = urllib.request.urlopen('http://data.pr4e.org/cover3.jpg')
+fhand = open('cover3.jpg', 'wb')
+size = 0
+while True:
+    info = img.read(100000)
+    if len(info) < 1: break
+    size = size + len(info)
+    fhand.write(info)
+    print(size, 'characters copied.')
+fhand.close()       
+
+# Search for link values within URL input
+import urllib.request, urllib.parse, urllib.error
+import re
+import ssl
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+url = input('Enter - ')
+html = urllib.request.urlopen(url).read()
+links = re.findall(b'href="(http[s]?://.*?)"', html)
+for link in links:
+    print(link.decode())
+
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+import ssl
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+url = input('Enter - ')
+html = urllib.request.urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, 'html.parser')
+# Retrieve all of the anchor tags
+tags = soup('a')
+for tag in tags:
+    print(tag.get('href', None))    
+
+pars = soup('p')
+for par in pars:
+    x = str(par)
+    print(x)
+    print(re.findall('\n(.*)\n', x))
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import ssl
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+url = input('Enter - ')
+html = urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, "html.parser")
+# Retrieve all of the anchor tags
+tags = soup('a')
+for tag in tags:
+# Look at the parts of a tag
+    print('TAG:', tag)
+    #12.9. BONUS SECTION FOR UNIX / LINUX USERS 153
+    print('URL:', tag.get('href', None))
+    print('Contents:', tag.contents[0])
+    print('Attrs:', tag.attrs)      
